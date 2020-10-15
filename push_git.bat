b919c57efa8bc96eb1ec49da47473cea1bb72dc5
m9810223 <39259397+m9810223@users.noreply.github.com>
2020-10-15 10:57:26 +0800

    Update push_git.bat

--- push_git.bat
+++ push_git.bat
@@ -1,10 +1,10 @@
 :start
 
-@set d="auto" 
+@set d="auto_push" 
 @set t=%date%%time%
 @set /p d=git commit: 
 
-@ git add . &&git commit -m "%t% %d%" && git push 
+@ git add . && git commit -m "%t% %d%" && git push 
 @rem pause
 @echo -----------------------------------------------------------------
 @goto start
